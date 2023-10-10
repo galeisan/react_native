135c06c4660d9973a7b35ae6e5c6905de07e13a6
galeisan <galeeva.leisan.27.09@yandex.ru>
2023-10-10 14:38:54 +0300

    hw3

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {View, Text, Button} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native' ;
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import TaskOne from "./TaskOne";
-import TaskTwo from "./TaskTwo";
-import TaskThree from "./TaskThree";
+import TaskOne from "./pages/TaskOne";
+import TaskTwo from "./pages/TaskTwo";
+import TaskThree from "./pages/TaskThree";
+import About from "./pages/About";
+import ToDoList from "./pages/ToDoList";
 
 
 const Stack = createNativeStackNavigator();
@@ -12,13 +14,16 @@ const Stack = createNativeStackNavigator();
 function HomeScreen({navigation}) {
     return (
         <View>
-            <Text>Main Page</Text>
             <Button onPress={() => navigation.navigate('Task One')}
-                    title="Task One"/>
+                    title="HW1.1"/>
             <Button onPress={() => navigation.navigate('Task Two')}
-                    title="Task Two"/>
+                    title="HW1.2"/>
             <Button onPress={() => navigation.navigate('Task Three')}
-                    title="Task Three"/>
+                    title="HW1.3"/>
+            <Button onPress={() => navigation.navigate('About')}
+                    title="HW2"/>
+            <Button onPress={() => navigation.navigate('ToDoList')}
+                    title="HW3"/>
         </View>
     );
 }
@@ -28,6 +33,8 @@ function App() {
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name={'Home'} component={HomeScreen}/>
+                <Stack.Screen name={'About'} component={About}/>
+                <Stack.Screen name={'ToDoList'} component={ToDoList}/>
                 <Stack.Screen name={'Task One'} component={TaskOne}/>
                 <Stack.Screen name={'Task Two'} component={TaskTwo}/>
                 <Stack.Screen name={'Task Three'} component={TaskThree}/>
